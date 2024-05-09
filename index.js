48789246b0918fdea446d52ded72a9366cf244f4
mizik1 <mattk29@gmail.com>
2024-05-09 13:48:45 -0500

    Added Express functionality
    - Added public folder
    - Added Express to index.js
    - Added top 10 movies list

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const { title } = require("process");
+const app = express();
+
+let topMovies = [
+  {
+    title: "Scarface",
+  },
+  {
+    title: "Goodwill Hunting",
+  },
+  {
+    title: "Boiler Room",
+  },
+  {
+    title: "Zoolander",
+  },
+  {
+    title: "Star Wars",
+  },
+  {
+    title: "Lost in Translation",
+  },
+  {
+    title: "Planes, Trains, and Automobiles",
+  },
+  {
+    title: "Forest Gump",
+  },
+  {
+    title: "One Flew Over The Cuckoo's Nest ",
+  },
+  {
+    title: "The Harder They Come",
+  },
+];
+
+// Get requests
+app.get("/movies", (req, res) => {
+  res.send("Welcome to my favorite movies!");
+});
+
+app.get("documentation", (req, res) => {
+  res.sendFile("public/documentation.html", {
+    root: __dirname,
+  });
+});
+
+app.get("/movies", (req, res) => {
+  res.json(topMovies);
+});
