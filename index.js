a42b4940a179f92def802e21699eb99d73cb51ae
mizik1 <mattk29@gmail.com>
2024-05-20 13:28:48 -0500

    Gave GenreIDs a number

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const movies = [
     Title: "Scarface",
     Description: "A riveting drama about the rise and fall of a notorius drug dealer.",
     Genre: "Drama",
+    // GenreID:1
     Director: {
       Name: "Brian De Palma",
       Bio: "Brian Russell De Palma is an American film director and screenwriter. With a career spanning over 50 years, he is best known for work in the suspense, crime and psychological thriller genres.",
@@ -32,6 +33,7 @@ const movies = [
     Description:
       "A touching tale of a young man who struggles to find his identity, living in a world where he can solve any problem, except the one brewing deep within himself.",
     Genre: "Drama",
+    // GenreID:1
     Director: {
       Name: "Gus Van Sant",
       Bio: "Gus Green Van Sant Jr. is an American film director, producer, photographer, and musician who has earned acclaim as an independent filmmaker.",
@@ -45,6 +47,7 @@ const movies = [
     Description:
       "A college dropout, attempting to live up to his father's high standards, gets a job as a broker for a suburban investment firm which puts him on the fast track to success. But the job might not be as legitimate as it first appeared to be.",
     Genre: "Drama",
+    // GenreID:1
     Director: {
       Name: "Ben Younger",
       Bio: "Younger became disenchanted with politics, and by 1995 started to seek a creative outlet that would rekindle the excitement he felt as a stand-up comedian. He wrote and directed a short film, as well as working on a number of feature films as a grip, and directing music videos and commercials.",
@@ -57,6 +60,7 @@ const movies = [
     Title: "Zoolander",
     Description: "At the end of his career, a clueless fashion model is brainwashed to kill the Prime Minister of Malaysia.",
     Genre: "Comedy",
+    // GenreID:2
     Director: {
       Name: "Ben Stiller",
       Bio: "Benjamin Edward Meara Stiller is an American actor, filmmaker, and comedian.",
@@ -70,6 +74,7 @@ const movies = [
     Description:
       "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire's world-destroying battle station, while also attempting to rescue Princess Leia from the mysterious Darth Vader.",
     Genre: "Sci-fi",
+    // GenreID:3
     Director: {
       Name: "George Lucas",
       Bio: "George Walton Lucas Jr.[1] (born May 14, 1944) is an American filmmaker and philanthropist. He created the Star Wars and Indiana Jones franchises and founded Lucasfilm, LucasArts, Industrial Light & Magic and THX.",
@@ -82,6 +87,7 @@ const movies = [
     Title: "Lost in Translation",
     Description: "A faded movie star and a neglected young woman form an unlikely bond after crossing paths in Tokyo.",
     Genre: "Drama",
+    // GenreID:1
     Director: {
       Name: "Sofia Coppola",
       Bio: "Sofia Carmina Coppola (/ˈkoʊpələ/ KOH-pəl-ə[1] Italian pronunciation: [soˈfiːa karˈmiːna ˈkoppola]; born May 14, 1971) is an American film director, screenwriter, producer, and former actress.",
@@ -95,6 +101,7 @@ const movies = [
     Description:
       "A Chicago advertising man must struggle to travel home from New York for Thanksgiving, with a lovable oaf of a shower-curtain-ring salesman as his only companion.",
     Genre: "Comedy",
+    // GenreID:2
     Director: {
       Name: "John Hughes",
       Bio: "John Wilden Hughes Jr. was an American film director, producer and screenwriter.",
@@ -108,6 +115,7 @@ const movies = [
     Description:
       "The history of the United States from the 1950s to the '70s unfolds from the perspective of an Alabama man with an IQ of 75, who yearns to be reunited with his childhood sweetheart.",
     Genre: "Comedy",
+    // GenreID:2
     Director: {
       Name: "Robert Zemeckis",
       Bio: "Robert Lee Zemeckis (born May 14, 1952) is an American filmmaker. He first came to public attention as the director of the action-adventure romantic comedy Romancing the Stone, the science-fiction comedy Back to the Future trilogy, and the live-action/animated comedy Who Framed Roger Rabbit.",
@@ -121,6 +129,7 @@ const movies = [
     Description: "A research team in Antarctica is hunted by a shape-shifting alien that assumes the appearance of its victims.",
     Genre: "Horror",
     Director: {
+      // GenreID:4
       Name: "John Carpenter",
       Bio: "John Howard Carpenter is an American filmmaker, composer, and actor. Most commonly associated with horror, action, and science fiction films of the 1970s and 1980s, he is generally recognized as a master of the horror genre.",
       BirthDate: "January 16, 1948",
