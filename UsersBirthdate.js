9ac7a4a8c01228ed8a437f06636cbad2fdbe8608
mizik1 <mattk29@gmail.com>
2024-05-28 13:18:47 -0500

    Installed mongoose and models.js
    Added movies.js and UsersBirthdate files as place to work with data

---
+++ UsersBirthdate.js
@@ -0,0 +1,39 @@
+const user1 = {
+  userID: 1,
+  name: "user1",
+  password: "password1",
+  email: "user1@example.com",
+  birthday: new Date("1990-01-01"),
+};
+
+const user2 = {
+  userID: 2,
+  name: "user2",
+  password: "password2",
+  email: "user2@example.com",
+  birthday: new Date("1992-02-02"),
+};
+
+const user3 = {
+  userID: 3,
+  name: "user3",
+  password: "password3",
+  email: "user3@example.com",
+  birthday: new Date("1994-03-03"),
+};
+
+const user4 = {
+  userID: 4,
+  name: "user4",
+  password: "password4",
+  email: "user4@example.com",
+  birthday: new Date("1996-04-04"),
+};
+
+const user5 = {
+  userID: 5,
+  name: "user5",
+  password: "password5",
+  email: "user5@example.com",
+  birthday: new Date("1998-05-05"),
+};
